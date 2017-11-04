22309174cf01a8259d99d934727931079d2bc174
Funatsu Ryuichiro <33192274+funatsu272@users.noreply.github.com>
2017-11-04 20:57:14 +0900

    Add files via upload

---
+++ BINGO/functions.php
@@ -0,0 +1,5 @@
+<?php
+
+functuion h($s) {
+	return htmlspecialchars($s,ENT_QUOTES, 'UTF-8');
+};
---
+++ BINGO/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>BINGO</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div id="container">
+		<table>
+			<tr>
+				<th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
+			</tr>
+			<tr>
+				<tb>1</tb>
+				<tb>1</tb>
+				<tb>1</tb>
+				<tb>1</tb>
+				<tb>1</tb>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
